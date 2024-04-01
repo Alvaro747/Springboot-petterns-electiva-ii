2afeb834f0a4cdbd427254802371bbfc4401bc76
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-04-01 07:54:56 -0500

    Add new repository interfaces for Estudiante, Materia, and Matricula

---
+++ src/main/java/com/corhuila/electivaii/IRepository/IEstudianteRepository.java
@@ -0,0 +1,9 @@
+package com.corhuila.electivaii.IRepository;
+
+import com.corhuila.electivaii.Entity.Estudiante;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEstudianteRepository extends IBaseRepository<Estudiante, Long>{
+
+}
---
+++ src/main/java/com/corhuila/electivaii/IRepository/IMateriaRepository.java
@@ -0,0 +1,9 @@
+package com.corhuila.electivaii.IRepository;
+
+import com.corhuila.electivaii.Entity.Materia;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMateriaRepository extends IBaseRepository<Materia, Long> {
+
+}
